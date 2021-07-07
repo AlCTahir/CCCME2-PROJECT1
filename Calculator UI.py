46f67a207a8fe954d5279c0c8c1e6bea26e89673
AlCTahir <86411164+AlCTahir@users.noreply.github.com>
2021-07-07 19:19:01 +0800

    Calculator UI

--- Calculator UI.py
+++ Calculator UI.py
@@ -4,12 +4,16 @@ calcu = Tk()
 
 
 calcu.title("Calculator") 
-
-
 calcu.resizable(0,0)
 
 backg=Frame(calcu, bg="white") 
 backg.pack() 
 
+equation=  StringVar()
+widg=Entry(backg,textvariable=equation,justify=RIGHT,font=("arial",20,"bold"),bg="black",fg="white")
+widg.pack()
+
+
+widg.grid(row=0,column=0,ipadx=10,columnspan = 4,ipady=20,pady=15,)
 
 calcu.mainloop()
